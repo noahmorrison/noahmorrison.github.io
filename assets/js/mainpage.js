edce103eeadfa46e4993d729363839ccac6170fc
noah morrison <noah@morrison.ph>
2014-11-30 21:42:01 -0500

    Updated resume and fixed some small bugs
    Bugs mostly dealing with anchor reloading and stuff
    (Look at https://github.com/noahmorrison/gitsite for more
    detailed commit messages)

--- assets/js/mainpage.js
+++ assets/js/mainpage.js
@@ -11,18 +11,26 @@ window.addEventListener('hashchange', function() {
         document.getElementById('pages').style.setProperty('display', 'block');
 
         // Update the current page (leak the variable to global scope)
-        _currentpage = document.getElementById(location.hash.substring(1));
+        _currentpage = document.getElementsByName(location.hash.substring(1))[0];
 
         // Show the current page
         _currentpage.style.setProperty('display', 'block');
 
-    // The hash didn't have data so we're going hide the page
+    // The hash didn't have data so we're going to show my resume
     } else {
         // Hide the pages (and the background)
-        document.getElementById('pages').style.setProperty('display', 'none');
+        location.hash = 'resume';
     }
 });
 
 window.addEventListener('load', function() {
+    var anchors = document.getElementsByTagName('a');
+    for(i = 0, len=anchors.length; i<len; i++){
+        anchors[i].addEventListener('click', function(e) {
+            location.hash = e.target.hash.substring(1);
+            e.preventDefault();
+        });
+    }
+
     window.dispatchEvent(new HashChangeEvent('hashchange'));
 });
